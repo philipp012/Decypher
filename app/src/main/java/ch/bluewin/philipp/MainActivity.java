034522a9c04b7dbc6f6e42b1daa3de5fed05b05e
philipp012 <philipp.kiss@bluewin.ch>
2019-09-22 17:51:36 +0200

    reworked image capturing process

--- app/src/main/java/ch/bluewin/philipp/MainActivity.java
+++ app/src/main/java/ch/bluewin/philipp/MainActivity.java
@@ -5,73 +5,25 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
-import android.os.Environment;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 public class MainActivity extends AppCompatActivity {
     private Context context = this;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView imageView;
-    private String currentPhotoPath;
-
-    private void dispatchTakePictureIntent() throws IOException {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            createImageFile();
-        }
-    }
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "Decoded_" + timeStamp;
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            assert extras != null;
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }
-
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // get imageview
-        ImageView imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
 
         // get solution button
         Button solutionBtn = findViewById(R.id.solutionButton);
@@ -113,16 +65,55 @@ public class MainActivity extends AppCompatActivity {
         decypherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    dispatchTakePictureIntent();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                takePicture(view);
             }
         });
 
     }
 
+    private void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = applyFilter(imageBitmap);
+
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+    private Bitmap applyFilter(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] data = new int[width * height];
+        bitmap.getPixels(data, 0, width, 0, 0, width, height);
+
+        // Hier können die Pixel im data-array bearbeitet und
+        // anschliessend damit ein neues Bitmap erstellt werden
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+
+
+
+                int pixel = bitmap.getPixel(i, j);
+                int green = Color.green(pixel);
+                int blue = Color.blue(pixel);
+
+                bitmap.setPixel(i, j, Color.argb(Color.alpha(pixel), 255, green, blue));
+            }
+        }
+
+        return Bitmap.createBitmap(data, width, height, Bitmap.Config.ARGB_8888);
+    }
 
     @Override
     protected void onResume() {
@@ -134,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    // Logging
     private void log(String logmessage) {
         Intent intent = new Intent("ch.appquest.intent.LOG");
 
