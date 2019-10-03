371e0d5d2148a3bf3206250b074bd90abe5d35cd
nico <nico.tuscano@adnovum.ch>
2019-10-03 16:31:49 +0200

    Start changing mainActivity to use full size image

--- app/src/main/java/ch/bluewin/philipp/MainActivity.java
+++ app/src/main/java/ch/bluewin/philipp/MainActivity.java
@@ -5,12 +5,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,9 +25,21 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Context context = this;
     private ImageView imageView;
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
 
     @Override
@@ -65,23 +82,43 @@ public class MainActivity extends AppCompatActivity {
         decypherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takePicture(view);
+               dispatchTakePictureIntent();
             }
         });
-
     }
 
-    private void takePicture(View view) {
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (imageTakeIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            /*assert data != null;
+            Uri contactUri = data.getData();
+            assert contactUri != null;
+            Bitmap bitmap = readImageFile(contactUri);
+            assert bitmap != null;
+            applyFilter(bitmap);*/
+
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             imageBitmap = applyFilter(imageBitmap);
@@ -91,6 +128,42 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private Bitmap readImageFile(Uri imageUri) {
+        File file = new File(imageUri.getPath());
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            return bitmap;
+        } catch (FileNotFoundException e) {
+            Log.e("DECODER", "Could not find image file", e);
+            return null;
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+
     private Bitmap applyFilter(Bitmap bitmap) {
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
@@ -98,55 +171,19 @@ public class MainActivity extends AppCompatActivity {
 
         bitmap.getPixels(data, 0, width, 0, 0, width, height);
 
-        int[] rgbteil = new int[3];
+        // Hier können die Pixel im data-array bearbeitet und
+        // anschliessend damit ein neues Bitmap erstellt werden
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixel = bitmap.getPixel(i, j);
+                int green = Color.green(pixel);
+                int blue = Color.blue(pixel);
 
-        int rot = Color.RED;
-
-        //get alpha
-        int rota = (rot >> 24) & 0xff;
-
-        //get red
-        int rotr = (rot >> 16) & 0xff;
-
-        //get green
-        int rotg = (rot >> 8) & 0xff;
-
-        //get blue
-        int rotb = rot & 0xff;
-
-
-        for (int i = 0; i < data.length; i++) {
-            //get alpha
-            int a = (data[i] >> 24) & 0xff;
-
-            //get red
-            int r = (data[i] >> 16) & 0xff;
-
-            //get green
-            int g = (data[i] >> 8) & 0xff;
-
-            //get blue
-            int b = data[i] & 0xff;
-
-            a *= rota;
-            r *= rotr;
-            g *= rotg;
-            b *= rotb;
-
-            data[i] = (a << 24) | (r << 16) | (g << 8) | b;
+                bitmap.setPixel(i, j, Color.argb(Color.alpha(pixel), 255, green, blue));
+            }
         }
 
-        return Bitmap.createBitmap(data, width, height, Bitmap.Config.ARGB_8888);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
+        return bitmap;
     }
 
     // Logging
@@ -173,5 +210,4 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
-
 }
